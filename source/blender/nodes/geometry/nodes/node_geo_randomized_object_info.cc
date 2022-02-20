d492aee7ede8d2a08d2f40cdbcaf071e1e66ab67
Jacob Lewallen <jlewallen@gmail.com>
2022-02-20 11:01:12 -0800

    refact: Scrambling around.

--- source/blender/nodes/geometry/nodes/node_geo_randomized_object_info.cc
+++ source/blender/nodes/geometry/nodes/node_geo_randomized_object_info.cc
@@ -7,7 +7,29 @@
 #include "UI_interface.h"
 #include "UI_resources.h"
 
+#include "DNA_modifier_types.h"
+
+#include "BKE_DerivedMesh.h"
 #include "BKE_collection.h"
+#include "BKE_idprop.h"
+#include "BKE_object.h"
+
+#include "DEG_depsgraph_build.h"
+#include "DEG_depsgraph_query.h"
+
+#include "NOD_derived_node_tree.hh"
+#include "NOD_geometry.h"
+#include "NOD_geometry_nodes_eval_log.hh"
+#include "NOD_node_declaration.hh"
+
+// #include "MOD_modifiertypes.h"
+// #include "MOD_nodes.h"
+// #include "MOD_nodes_evaluator.hh"
+// #include "MOD_ui_common.h"
+
+#include "FN_field.hh"
+#include "FN_field_cpp_type.hh"
+#include "FN_multi_function.hh"
 
 #include "node_geometry_util.hh"
 
@@ -42,12 +64,105 @@ static void node_geo_exec(GeoNodeExecParams params)
   const bool transform_space_relative = (storage.transform_space ==
                                          GEO_NODE_TRANSFORM_SPACE_RELATIVE);
 
-  Object *object = params.get_input<Object *>("Object");
-
   const Object *self_object = params.self_object();
+  const int seed = params.get_input<int>("Seed");
+
+  printf("\nOK: seed=%d\n", seed);
+
+  Object *object = params.get_input<Object *>("Object");
   if (object == nullptr) {
     params.set_default_remaining_outputs();
     return;
+  }
+
+  auto modified = false;
+
+  LISTBASE_FOREACH (ModifierData *, md, &object->modifiers) {
+    if (md->type == eModifierType_Nodes) {
+      NodesModifierData *nmd = (NodesModifierData *)md;
+      if (nmd->node_group != nullptr) {
+        printf("NODE-GROUP: NODE-GROUP:\n");
+
+        int socket_index;
+        LISTBASE_FOREACH_INDEX (bNodeSocket *, socket, &nmd->node_group->inputs, socket_index) {
+          printf("NODE-GROUP: SOCKET: '%s' identifier='%s'\n", socket->name, socket->identifier);
+
+          IDProperty *property = IDP_GetPropertyFromGroup(nmd->settings.properties,
+                                                          socket->identifier);
+          if (property != nullptr) {
+            if (property->type == IDP_INT) {
+              printf("NODE-GROUP: [%p] seed property\n", property);
+              IDP_Int(property) = seed;
+              modified = true;
+            }
+          }
+        }
+
+        /*
+        IDP_foreach_property(
+            nmd->settings.properties,
+            0,
+            [](IDProperty *property, void *user_data) {
+              ID *id = IDP_Id(property);
+              char *repr = IDP_reprN(property, NULL);
+              printf("IDProperty(%p): ", property);
+              if (repr != NULL) {
+                printf("%s\n", repr);
+                MEM_freeN(repr);
+              }
+              else {
+                printf("<null>\n");
+              }
+
+              if (property->type == IDP_FLOAT) {
+                float value = IDP_Float(property);
+                printf("Property-F: %f\n", value);
+              }
+              if (property->type == IDP_INT) {
+                int value = IDP_Int(property);
+                printf("Property-D: %d\n", value);
+              }
+            },
+            nullptr);
+            */
+
+        /*
+        // Unavailable from this level of the source tree, appears this is only available from MOD_
+        layer.
+
+        NodeTreeRefMap tree_refs;
+        DerivedNodeTree tree{*nmd->node_group, tree_refs};
+
+        blender::nodes::NodeMultiFunctions mf_by_node{tree};
+        Map<DOutputSocket, GMutablePointer> group_inputs;
+        Vector<DInputSocket> group_outputs;
+        std::optional<geo_log::GeoLogger> geo_logger;
+
+        blender::modifiers::geometry_nodes::GeometryNodesEvaluationParams eval_params;
+
+        eval_params.input_values = group_inputs;
+        eval_params.output_sockets = group_outputs;
+        eval_params.mf_by_node = &mf_by_node;
+        eval_params.modifier_ = nmd;
+        eval_params.depsgraph = params.depsgraph();
+        eval_params.self_object = object;
+        eval_params.geo_logger = geo_logger.has_value() ? &*geo_logger : nullptr;
+        blender::modifiers::geometry_nodes::evaluate_geometry_nodes(eval_params);
+        GeometrySet output_geometry_set = std::move(
+            *eval_params.r_output_values[0].get<GeometrySet>());
+            */
+      }
+    }
+  }
+
+  if (modified) {
+    Scene *scene = DEG_get_input_scene(params.depsgraph());
+    printf("modified, building object data\n");
+
+    // Crashes on memory free, likely from recurisve call into BKE_ layer
+    // makeDerivedMesh(params.depsgraph(), scene, object, &CD_MASK_BAREMESH);
+
+    printf("modified, building object data, done\n");
   }
 
   const float4x4 &object_matrix = object->obmat;
@@ -68,97 +183,6 @@ static void node_geo_exec(GeoNodeExecParams params)
 
     params.set_output("Geometry", geometry_set);
   }
-
-  /*
-  Collection *collection = params.get_input<Collection *>("Collection");
-
-  if (collection == nullptr) {
-    params.set_default_remaining_outputs();
-    return;
-  }
-  const Object *self_object = params.self_object();
-  const bool is_recursive = BKE_collection_has_object_recursive_instanced(collection,
-                                                                          (Object *)self_object);
-  if (is_recursive) {
-    params.error_message_add(NodeWarningType::Error, "Collection contains current object");
-    params.set_default_remaining_outputs();
-    return;
-  }
-
-  const NodeGeometryCollectionInfo &storage = node_storage(params.node());
-  const bool use_relative_transform = (storage.transform_space ==
-                                       GEO_NODE_TRANSFORM_SPACE_RELATIVE);
-
-  GeometrySet geometry_set_out;
-  InstancesComponent &instances = geometry_set_out.get_component_for_write<InstancesComponent>();
-
-  const bool separate_children = params.get_input<bool>("Separate Children");
-  if (separate_children) {
-    const bool reset_children = params.get_input<bool>("Reset Children");
-    Vector<Collection *> children_collections;
-    LISTBASE_FOREACH (CollectionChild *, collection_child, &collection->children) {
-      children_collections.append(collection_child->collection);
-    }
-    Vector<Object *> children_objects;
-    LISTBASE_FOREACH (CollectionObject *, collection_object, &collection->gobject) {
-      children_objects.append(collection_object->ob);
-    }
-
-    instances.reserve(children_collections.size() + children_objects.size());
-    Vector<InstanceListEntry> entries;
-    entries.reserve(children_collections.size() + children_objects.size());
-
-    for (Collection *child_collection : children_collections) {
-      float4x4 transform = float4x4::identity();
-      if (!reset_children) {
-        add_v3_v3(transform.values[3], child_collection->instance_offset);
-        if (use_relative_transform) {
-          mul_m4_m4_pre(transform.values, self_object->imat);
-        }
-        else {
-          sub_v3_v3(transform.values[3], collection->instance_offset);
-        }
-      }
-      const int handle = instances.add_reference(*child_collection);
-      entries.append({handle, &(child_collection->id.name[2]), transform});
-    }
-    for (Object *child_object : children_objects) {
-      const int handle = instances.add_reference(*child_object);
-      float4x4 transform = float4x4::identity();
-      if (!reset_children) {
-        if (use_relative_transform) {
-          transform = self_object->imat;
-        }
-        else {
-          sub_v3_v3(transform.values[3], collection->instance_offset);
-        }
-        mul_m4_m4_post(transform.values, child_object->obmat);
-      }
-      entries.append({handle, &(child_object->id.name[2]), transform});
-    }
-
-    std::sort(entries.begin(),
-              entries.end(),
-              [](const InstanceListEntry &a, const InstanceListEntry &b) {
-                return BLI_strcasecmp_natural(a.name, b.name) <= 0;
-              });
-    for (const InstanceListEntry &entry : entries) {
-      instances.add_instance(entry.handle, entry.transform);
-    }
-  }
-  else {
-    float4x4 transform = float4x4::identity();
-    if (use_relative_transform) {
-      copy_v3_v3(transform.values[3], collection->instance_offset);
-      mul_m4_m4_pre(transform.values, self_object->imat);
-    }
-
-    const int handle = instances.add_reference(*collection);
-    instances.add_instance(handle, transform);
-  }
-
-  params.set_output("Geometry", geometry_set_out);
-  */
 }
 
 }  // namespace blender::nodes::node_geo_randomized_object_info_cc
